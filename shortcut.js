282f1b43d492d02b6bc469bc6fa2b3465329569d
Anderbone <yanchiyu@outlook.com>
2022-06-06 23:15:52 +0100

    add clean, example, retrieve shortcut

--- shortcut.js
+++ shortcut.js
@@ -1,13 +1,20 @@
-document.onkeyup = function(e) {
-	// ctrl + enter key binding on chrome
-	if(e.ctrlKey && e.key === 'Enter'){
-	document.querySelectorAll('[ data-cy="submit-code-btn"]')[0].click()
+document.onkeyup = function (e) {
+	// ctrl + enter key to submit
+	if (e.ctrlKey && e.key === 'Enter') {
+		document.querySelectorAll('[ data-cy="submit-code-btn"]')[0].click()
 	}
-	// mac chrome cmd + enter
-	// var key = e.key;
-	// var cmd_held = e.metaKey;
-	// if(cmd_held && e.keyCode === 13){
-	// document.querySelectorAll('[ data-cy="submit-code-btn"]')[0].click()
-	// }
-	
+	// ctrl + atl + l to clean
+	else if (e.ctrlKey && e.altKey && e.key === 'l') {
+		document.querySelectorAll('.tool-button__2cBA')[2].click();
+		document.evaluate("//div[text()='Your code will be discarded and reset to the default code!']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.nextSibling.childNodes[1].click();
 	}
+	// ctrl + ; to use example testcases
+	else if (e.ctrlKey && e.key === ';') {
+		document.querySelectorAll('.example-testcases__3q5c')[0].click()
+	}
+	// ctrl + m to retrieve the last submission
+	else if (e.ctrlKey && e.key === 'm') {
+		document.querySelectorAll('.tool-button__2cBA')[1].click();
+		document.evaluate("//div[contains(text(),'replaced with your last submission')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.nextSibling.childNodes[1].click();
+	}
+}
